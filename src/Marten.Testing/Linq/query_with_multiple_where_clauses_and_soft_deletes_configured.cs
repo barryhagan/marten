dd95f38bc9c47b326310b4f47ac2c3a4c281501c
Barry Hagan <barryhagan@gmail.com>
2017-08-18 15:52:58 -0500

    Failing test for #854

--- src/Marten.Testing/Linq/query_with_multiple_where_clauses_and_soft_deletes_configured.cs
+++ src/Marten.Testing/Linq/query_with_multiple_where_clauses_and_soft_deletes_configured.cs
@@ -34,6 +34,31 @@ namespace Marten.Testing.Linq
         }
 
         [Fact]
+        public async Task TestWhereOrClauseWithSoftDeleteConfigured()
+        {
+            StoreOptions(_ => _.Schema.For<SoftDeletedItem>().SoftDeleted());
+
+            var item1 = new SoftDeletedItem { Number = 1, Name = "Jim Bob" };
+            var item2 = new SoftDeletedItem { Number = 2, Name = "Joe Bill" };
+            var item3 = new SoftDeletedItem { Number = 3, Name = "Jim Beam" };
+
+            using (var session = theStore.OpenSession())
+            {
+                session.Store(item1, item2, item3);
+                session.SaveChanges();
+            }
+
+            using (var session = theStore.QuerySession())
+            {
+                var query = session.Query<SoftDeletedItem>()
+                    .Where(x => x.Number == 1 || x.Number == 2);
+
+                var result = await query.ToListAsync();
+                Assert.Equal(2, result.Count);
+            }
+        }
+
+        [Fact]
         public async Task TestMultipleWhereClausesWithoutSoftDeleteConfigured()
         {
             StoreOptions(_ => _.Schema.For<SoftDeletedItem>());
